a162aa6b8eb8a490b9f6ee1712c327fe3989160a
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2023-12-18 13:05:59 -0600

    Rando Char
    With atleast one checkbox tickd, the generate button request and displays a random character from the backend.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,20 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::{io, cmp::Ordering, char::from_digit};
+use rand::Rng;
+
+// Very helpful post here: https://stackoverflow.com/questions/53971954/why-cant-a-range-of-char-be-collected
+//33..=47 are special chars
+//48..=57 are numbers
+//65..=90 are uppercase
+//97..=122 are lowercase
+
 #[tauri::command]
-fn greet() -> String {
-  format!("Howdy I Work wheeee")
+fn greet(len: u8, lwr: bool, upr: bool, numbr: bool, specl: bool) -> char {
+  let rando_number: u8 = rand::thread_rng().gen_range(33..=122);
+  let numbo: char = rando_number as char;
+  numbo
 }
 
 fn main() {
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,9 @@
 	import '../app.postcss';
 
 	// Floating UI for Popups
-	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
-	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	// import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	// import { storePopup } from '@skeletonlabs/skeleton';
+	// storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
 <slot />
